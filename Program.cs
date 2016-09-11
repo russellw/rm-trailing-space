633ccb37991e642297917349e213301126972eb9
Russell Wallace <russell.wallace@gmail.com>
2016-09-11 15:07:48 +0100

    encoding

--- Program.cs
+++ Program.cs
@@ -11,37 +11,37 @@ namespace rm_trailing_space
     {
         static bool isBinary(string path)
         {
-            var content = File.ReadAllBytes(path);
-            foreach (var x in content)
-                if (x == 0)
+            var bytes = File.ReadAllBytes(path);
+            foreach (var b in bytes)
+                if (b == 0)
                     return true;
             return false;
         }
 
         static void Main(string[] args)
         {
-            foreach(var path in args)
+            foreach (var path in args)
             {
-                if(isBinary(path))
+                if (isBinary(path))
                 {
                     Console.WriteLine("{0}: binary file", path);
                     continue;
                 }
-                var lines = File.ReadAllLines(path, Encoding.UTF8);
+                var contents = File.ReadAllLines(path);
                 var changed = false;
-                for(int i=0;i<lines.Length;i++)
+                for (int i = 0; i < contents.Length; i++)
                 {
-                    var s = lines[i].TrimEnd(null);
-                    if(s!=lines[i])
+                    var s = contents[i].TrimEnd(null);
+                    if (s != contents[i])
                     {
-                        lines[i] = s;
+                        contents[i] = s;
                         changed = true;
                     }
                 }
-                if(changed)
+                if (changed)
                 {
                     Console.WriteLine(path);
-                    File.WriteAllLines(path, lines, Encoding.UTF8);
+                    File.WriteAllLines(path, contents);
                 }
             }
         }
