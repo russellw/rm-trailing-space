7583acde3ffdcdde48b9bfaa0b53ee4ea355caee
Russell Wallace <russell.wallace@gmail.com>
2016-09-11 14:41:34 +0100

    isbinary

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,40 @@ namespace rm_trailing_space
 {
     class Program
     {
+        static bool isBinary(string path)
+        {
+            var content = File.ReadAllBytes(path);
+            foreach (var x in content)
+                if (x == 0)
+                    return true;
+            return false;
+        }
+
         static void Main(string[] args)
         {
-            foreach(var filename in args)
+            foreach(var path in args)
             {
-                Console.WriteLine(filename);
+                if(isBinary(path))
+                {
+                    Console.WriteLine("{0}: binary file", path);
+                    continue;
+                }
+                var lines = File.ReadAllLines(path, Encoding.UTF8);
+                var changed = false;
+                for(int i=0;i<lines.Length;i++)
+                {
+                    var s = lines[i].TrimEnd(null);
+                    if(s!=lines[i])
+                    {
+                        lines[i] = s;
+                        changed = true;
+                    }
+                }
+                if(changed)
+                {
+                    Console.WriteLine(path);
+                    File.WriteAllLines(path, lines, Encoding.UTF8);
+                }
             }
         }
     }
